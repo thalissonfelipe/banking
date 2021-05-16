a4acaaf8a2f7821b25aea36d86d4aa57c492dd75
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-05-16 19:00:07 -0300

    feat: ensure GetAccountBalanceByID returns an error if repository fails to fetch

--- pkg/domain/account/usecase/get_balance_by_id.go
+++ pkg/domain/account/usecase/get_balance_by_id.go
@@ -4,5 +4,8 @@ import "context"
 
 func (a Account) GetAccountBalanceByID(ctx context.Context, accountID string) (int, error) {
 	balance, err := a.repository.GetBalanceByID(ctx, accountID)
-	return balance, err
+	if err != nil {
+		return 0, err
+	}
+	return balance, nil
 }
