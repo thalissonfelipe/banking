c74a724864cf8d706cb11c4804ecaafe19938ed5
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-05-16 18:18:07 -0300

    feat: add ListAccounts function to Account type

---
+++ pkg/domain/account/usecase/list_accounts.go
@@ -0,0 +1,11 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/thalissonfelipe/banking/pkg/domain/entities"
+)
+
+func (a Account) ListAccounts(ctx context.Context) ([]entities.Account, error) {
+	return nil, nil
+}
