cded75ddcebcdb0cb8b135674c8a0eb28dca1d7d
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-05-16 18:44:24 -0300

    test: ensure ListAccounts usecase returns an error if repository fails to fetch

--- pkg/domain/account/usecase/list_accounts_test.go
+++ pkg/domain/account/usecase/list_accounts_test.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,9 +11,13 @@ import (
 
 type StubRepository struct {
 	accounts []entities.Account
+	err      error
 }
 
 func (s StubRepository) GetAccounts(ctx context.Context) ([]entities.Account, error) {
+	if s.err != nil {
+		return nil, s.err
+	}
 	return s.accounts, nil
 }
 
@@ -21,16 +26,25 @@ func (s StubRepository) GetBalanceByID(ctx context.Context, id string) (int, err
 }
 
 func TestListAccounts(t *testing.T) {
-	acc := entities.NewAccount("Piter", "12345678", "123.456.789-00")
-	repo := StubRepository{accounts: []entities.Account{acc}}
-	usecase := Account{&repo}
 	ctx := context.Background()
 
 	t.Run("should return a list of accounts", func(t *testing.T) {
+		acc := entities.NewAccount("Piter", "12345678", "123.456.789-00")
+		repo := StubRepository{accounts: []entities.Account{acc}, err: nil}
+		usecase := Account{&repo}
 		expected := []entities.Account{acc}
 		result, err := usecase.ListAccounts(ctx)
 
 		assert.Nil(t, err)
 		assert.Equal(t, expected, result)
+	})
+
+	t.Run("should return an error if something went wrong on repository", func(t *testing.T) {
+		repo := StubRepository{accounts: nil, err: errors.New("failed to fetch accounts")}
+		usecase := Account{&repo}
+		result, err := usecase.ListAccounts(ctx)
+
+		assert.Nil(t, result)
+		assert.NotNil(t, err)
 	})
 }
