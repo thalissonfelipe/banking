0c3f2c0c2070c88177bf713fe8e6c2e0b5e84e4d
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-05-17 09:00:25 -0300

    feat: add CreateAccount declaration and CreateAccountInput struct

--- pkg/domain/account/usecase.go
+++ pkg/domain/account/usecase.go
@@ -9,5 +9,11 @@ import (
 type UseCase interface {
 	ListAccounts(ctx context.Context) ([]entities.Account, error)
 	GetAccountBalanceByID(ctx context.Context, accountID string) (int, error)
-	// TODO: CreateAccount
+	CreateAccount(ctx context.Context, input CreateAccountInput) (entities.Account, error)
+}
+
+type CreateAccountInput struct {
+	Name   string
+	CPF    string
+	Secret string
 }
