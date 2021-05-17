7eb65600bda81b7612414527970cd63d9e199c04
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-05-17 16:12:42 -0300

    feat: add ListAccounts body function

---
+++ pkg/gateways/http/account/account.go
@@ -0,0 +1,27 @@
+package account
+
+import (
+	"time"
+
+	"github.com/thalissonfelipe/banking/pkg/domain/entities"
+)
+
+type AccountResponse struct {
+	Name      string
+	CPF       string
+	Balance   int
+	CreatedAt string
+}
+
+func convertAccountToAccountResponse(account entities.Account) AccountResponse {
+	return AccountResponse{
+		Name:      account.Name,
+		CPF:       account.CPF,
+		Balance:   account.Balance,
+		CreatedAt: formatTime(account.CreatedAt),
+	}
+}
+
+func formatTime(t time.Time) string {
+	return t.Format("2006-01-02 15:04:05.000000")
+}
--- pkg/gateways/http/account/list_accounts.go
+++ pkg/gateways/http/account/list_accounts.go
@@ -1,9 +1,21 @@
 package account
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
 func (h Handler) ListAccounts(w http.ResponseWriter, r *http.Request) {
+	accounts, err := h.usecase.ListAccounts(r.Context())
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("Internal Error."))
+	}
 
+	var accountsResponse []AccountResponse
+	for _, acc := range accounts {
+		accountsResponse = append(accountsResponse, convertAccountToAccountResponse(acc))
+	}
+
+	json.NewEncoder(w).Encode(accountsResponse)
 }
