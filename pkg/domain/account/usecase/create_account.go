ac15a9b17f9eadb9e1c117269841980826013bfc
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-05-17 09:28:07 -0300

    feat: ensure CreateAccount returns an error if repo fails to save

--- pkg/domain/account/usecase/create_account.go
+++ pkg/domain/account/usecase/create_account.go
@@ -9,5 +9,8 @@ import (
 
 func (a Account) CreateAccount(ctx context.Context, input account.CreateAccountInput) (*entities.Account, error) {
 	acc, err := a.repository.PostAccount(ctx, input)
+	if err != nil {
+		return nil, err
+	}
 	return acc, err
 }
