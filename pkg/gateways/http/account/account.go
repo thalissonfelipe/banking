c947d28a88f1550e7a1bdd1308e0340dd3cb7207
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-05-19 18:04:02 -0300

    feat: add CreateAccount handler

--- pkg/gateways/http/account/account.go
+++ pkg/gateways/http/account/account.go
@@ -1,9 +1,19 @@
 package account
 
 import (
+	"errors"
 	"time"
 
 	"github.com/thalissonfelipe/banking/pkg/domain/entities"
+)
+
+var (
+	errMissingNameParameter   = errors.New("missing name parameter")
+	errMissingCPFParameter    = errors.New("missing cpf parameter")
+	errMissingSecretParameter = errors.New("missing secret parameter")
+	errInvalidJSON            = errors.New("invalid json")
+	errInvalidCPF             = errors.New("invalid cpf")
+	errInvalidSecret          = errors.New("invalid secret")
 )
 
 type accountResponse struct {
@@ -17,12 +27,46 @@ type balanceResponse struct {
 	Balance int
 }
 
+type requestBody struct {
+	Name   string
+	CPF    string
+	Secret string
+}
+
+type createdAccountResponse struct {
+	Name    string
+	CPF     string
+	Balance int
+}
+
+func (r requestBody) isValid() error {
+	if r.Name == "" {
+		return errMissingNameParameter
+	}
+	if r.CPF == "" {
+		return errMissingCPFParameter
+	}
+	if r.Secret == "" {
+		return errMissingSecretParameter
+	}
+
+	return nil
+}
+
 func convertAccountToAccountResponse(account entities.Account) accountResponse {
 	return accountResponse{
 		Name:      account.Name,
 		CPF:       account.CPF.String(),
 		Balance:   account.Balance,
 		CreatedAt: formatTime(account.CreatedAt),
+	}
+}
+
+func convertAccountToCreatedAccountResponse(account *entities.Account) createdAccountResponse {
+	return createdAccountResponse{
+		Name:    account.Name,
+		CPF:     account.CPF.String(),
+		Balance: account.Balance,
 	}
 }
 
---
+++ pkg/gateways/http/account/create_account.go
@@ -0,0 +1,52 @@
+package account
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/thalissonfelipe/banking/pkg/domain/account"
+	"github.com/thalissonfelipe/banking/pkg/domain/entities"
+	"github.com/thalissonfelipe/banking/pkg/domain/vos"
+	"github.com/thalissonfelipe/banking/pkg/gateways/http/responses"
+)
+
+func (h Handler) CreateAccount(w http.ResponseWriter, r *http.Request) {
+	var body requestBody
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		responses.SendError(w, http.StatusBadRequest, errInvalidJSON.Error())
+	}
+
+	if err := body.isValid(); err != nil {
+		responses.SendError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	cpf := vos.NewCPF(body.CPF)
+	if ok := cpf.IsValid(); !ok {
+		responses.SendError(w, http.StatusBadRequest, errInvalidCPF.Error())
+		return
+	}
+
+	secret := vos.NewSecret(body.Secret)
+	if ok := secret.IsValid(); !ok {
+		responses.SendError(w, http.StatusBadRequest, errInvalidSecret.Error())
+		return
+	}
+
+	input := account.NewCreateAccountInput(body.Name, cpf, secret)
+	acc, err := h.usecase.CreateAccount(r.Context(), input)
+	if err != nil {
+		if errors.Is(err, entities.ErrAccountAlreadyExists) {
+			responses.SendError(w, http.StatusConflict, err.Error())
+			return
+		}
+
+		responses.SendError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	accResponse := convertAccountToCreatedAccountResponse(acc)
+	responses.SendJSON(w, http.StatusCreated, accResponse)
+}
