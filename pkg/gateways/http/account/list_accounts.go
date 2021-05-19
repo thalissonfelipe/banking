6ab4b188c5881af3d992bbb58f97f7b59da64d8f
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-05-19 17:15:10 -0300

    feat: change response body on get accounts handler

--- pkg/gateways/http/account/list_accounts.go
+++ pkg/gateways/http/account/list_accounts.go
@@ -9,7 +9,7 @@ import (
 func (h Handler) ListAccounts(w http.ResponseWriter, r *http.Request) {
 	accounts, err := h.usecase.ListAccounts(r.Context())
 	if err != nil {
-		responses.SendError(w, http.StatusInternalServerError, "Internal Error.")
+		responses.SendError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
