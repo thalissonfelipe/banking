e919f1267a8e9147e91a8616497eb3e296ca950a
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-05-19 18:04:17 -0300

    test: add unit tests for CreateAccount handler

---
+++ pkg/gateways/http/account/create_account_test.go
@@ -0,0 +1,157 @@
+package account
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+	"github.com/thalissonfelipe/banking/pkg/domain/account/usecase"
+	"github.com/thalissonfelipe/banking/pkg/domain/entities"
+	"github.com/thalissonfelipe/banking/pkg/domain/vos"
+	"github.com/thalissonfelipe/banking/pkg/gateways/http/responses"
+	"github.com/thalissonfelipe/banking/pkg/tests"
+	"github.com/thalissonfelipe/banking/pkg/tests/fakes"
+	"github.com/thalissonfelipe/banking/pkg/tests/mocks"
+)
+
+func TestCreateAccount(t *testing.T) {
+	testCases := []struct {
+		name         string
+		repo         *mocks.StubAccountRepository
+		enc          *mocks.StubHash
+		decoder      tests.Decoder
+		body         interface{}
+		expectedBody interface{}
+		expectedCode int
+	}{
+		{
+			name:         "should return status code 400 if name was not provided",
+			repo:         &mocks.StubAccountRepository{},
+			enc:          &mocks.StubHash{},
+			decoder:      tests.ErrorMessageDecoder{},
+			body:         requestBody{CPF: "123.456.789-00", Secret: "12345678"},
+			expectedBody: responses.ErrorResponse{Message: "missing name parameter"},
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name:         "should return status code 400 if cpf was not provided",
+			repo:         &mocks.StubAccountRepository{},
+			enc:          &mocks.StubHash{},
+			decoder:      tests.ErrorMessageDecoder{},
+			body:         requestBody{Name: "Pedro", Secret: "12345678"},
+			expectedBody: responses.ErrorResponse{Message: "missing cpf parameter"},
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name:         "should return status code 400 if secret was not provided",
+			repo:         &mocks.StubAccountRepository{},
+			enc:          &mocks.StubHash{},
+			decoder:      tests.ErrorMessageDecoder{},
+			body:         requestBody{Name: "Pedro", CPF: "123.456.789-00"},
+			expectedBody: responses.ErrorResponse{Message: "missing secret parameter"},
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name:         "should return status code 400 if an invalid json was provided",
+			repo:         &mocks.StubAccountRepository{},
+			enc:          &mocks.StubHash{},
+			decoder:      tests.ErrorMessageDecoder{},
+			body:         map[string]interface{}{"name": 123456},
+			expectedBody: responses.ErrorResponse{Message: "invalid json"},
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name:         "should return status code 400 if cpf provided is not valid",
+			repo:         &mocks.StubAccountRepository{},
+			enc:          &mocks.StubHash{},
+			decoder:      tests.ErrorMessageDecoder{},
+			body:         requestBody{Name: "Pedro", CPF: "123.456.789-00", Secret: "12345678"},
+			expectedBody: responses.ErrorResponse{Message: "invalid cpf"},
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name:         "should return status code 400 if secret provided is not valid",
+			repo:         &mocks.StubAccountRepository{},
+			enc:          &mocks.StubHash{},
+			decoder:      tests.ErrorMessageDecoder{},
+			body:         requestBody{Name: "Pedro", CPF: "648.446.967-93", Secret: "12345678"},
+			expectedBody: responses.ErrorResponse{Message: "invalid secret"},
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name: "should return status code 409 if cpf already exists",
+			repo: &mocks.StubAccountRepository{
+				Accounts: []entities.Account{entities.NewAccount(
+					"Junior", vos.NewCPF("648.446.967-93"), vos.NewSecret("aS1234Dz"),
+				)},
+			},
+			enc:          &mocks.StubHash{},
+			decoder:      tests.ErrorMessageDecoder{},
+			body:         requestBody{Name: "Pedro", CPF: "648.446.967-93", Secret: "As1234dZ"},
+			expectedBody: responses.ErrorResponse{Message: "account already exists"},
+			expectedCode: http.StatusConflict,
+		},
+		{
+			name: "should return status code 500 if usecase fails to create account",
+			repo: &mocks.StubAccountRepository{
+				Err: errors.New("usecase error"),
+			},
+			enc:          &mocks.StubHash{},
+			decoder:      tests.ErrorMessageDecoder{},
+			body:         requestBody{Name: "Pedro", CPF: "648.446.967-93", Secret: "As1234dZ"},
+			expectedBody: responses.ErrorResponse{Message: "internal server error"},
+			expectedCode: http.StatusInternalServerError,
+		},
+		{
+			name: "should return status code 500 if hash fails to hash secret",
+			repo: &mocks.StubAccountRepository{
+				Err: errors.New("usecase error"),
+			},
+			enc:          &mocks.StubHash{Err: errors.New("hash error")},
+			decoder:      tests.ErrorMessageDecoder{},
+			body:         requestBody{Name: "Pedro", CPF: "648.446.967-93", Secret: "As1234dZ"},
+			expectedBody: responses.ErrorResponse{Message: "internal server error"},
+			expectedCode: http.StatusInternalServerError,
+		},
+		{
+			name:         "should return status code 201 and created account",
+			repo:         &mocks.StubAccountRepository{},
+			enc:          &mocks.StubHash{},
+			decoder:      createdAccountDecoder{},
+			body:         requestBody{Name: "Pedro", CPF: "648.446.967-93", Secret: "As1234dZ"},
+			expectedBody: createdAccountResponse{Name: "Pedro", CPF: "648.446.967-93", Balance: 0},
+			expectedCode: http.StatusCreated,
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			r := mux.NewRouter()
+			usecase := usecase.NewAccountUseCase(tt.repo, tt.enc)
+			handler := NewHandler(r, usecase)
+
+			request := fakes.FakeRequest(http.MethodPost, "/accounts", tt.body)
+			response := httptest.NewRecorder()
+
+			http.HandlerFunc(handler.CreateAccount).ServeHTTP(response, request)
+
+			result := tt.decoder.Decode(response.Body)
+
+			assert.Equal(t, tt.expectedBody, result)
+			assert.Equal(t, tt.expectedCode, response.Code)
+		})
+	}
+}
+
+type createdAccountDecoder struct{}
+
+func (createdAccountDecoder) Decode(body *bytes.Buffer) interface{} {
+	var result createdAccountResponse
+	json.NewDecoder(body).Decode(&result)
+	return result
+}
