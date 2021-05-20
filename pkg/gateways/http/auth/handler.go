9e8ed673b11487320502683e1d0acb4016c23c07
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-05-19 21:42:26 -0300

    feat: add Login handler

---
+++ pkg/gateways/http/auth/handler.go
@@ -0,0 +1,22 @@
+package auth
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/thalissonfelipe/banking/pkg/services/auth"
+)
+
+type Handler struct {
+	authService *auth.Auth
+}
+
+func NewHandler(r *mux.Router, authService *auth.Auth) *Handler {
+	handler := &Handler{
+		authService: authService,
+	}
+
+	r.HandleFunc("/login", handler.Login).Methods(http.MethodPost)
+
+	return handler
+}
