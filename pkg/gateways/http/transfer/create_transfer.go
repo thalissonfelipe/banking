8f7541397daf2b162148fa49a4dea08b409a3ea3
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-05-20 14:38:34 -0300

    feat: add CreateTransfer handler

---
+++ pkg/gateways/http/transfer/create_transfer.go
@@ -0,0 +1,56 @@
+package transfer
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/thalissonfelipe/banking/pkg/domain/entities"
+	"github.com/thalissonfelipe/banking/pkg/domain/transfer"
+	"github.com/thalissonfelipe/banking/pkg/gateways/http/responses"
+	"github.com/thalissonfelipe/banking/pkg/services/auth"
+)
+
+func (h Handler) CreateTransfer(w http.ResponseWriter, r *http.Request) {
+	var body transferRequest
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		responses.SendError(w, http.StatusBadRequest, errInvalidJSON.Error())
+		return
+	}
+
+	if err := body.isValid(); err != nil {
+		responses.SendError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	token := getTokenFromHeader(r.Header.Get("Authorization"))
+	accountID := auth.GetIDFromToken(token)
+	input := transfer.NewTransferInput(accountID, body.AccountDestinationID, body.Amount)
+
+	err = h.usecase.CreateTransfer(r.Context(), input)
+	if err != nil {
+		if errors.Is(err, entities.ErrAccountDoesNotExist) {
+			responses.SendError(w, http.StatusNotFound, errAccountOriginDoesNotExist.Error())
+			return
+		}
+		if errors.Is(err, entities.ErrAccountDestinationDoesNotExist) {
+			responses.SendError(w, http.StatusNotFound, err.Error())
+			return
+		}
+		if errors.Is(err, entities.ErrInsufficientFunds) {
+			responses.SendError(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		responses.SendError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	response := transferCreatedResponse{
+		AccountOriginID:      accountID,
+		AccountDestinationID: body.AccountDestinationID,
+		Amount:               body.Amount,
+	}
+	responses.SendJSON(w, http.StatusOK, response)
+}
--- pkg/gateways/http/transfer/transfer.go
+++ pkg/gateways/http/transfer/transfer.go
@@ -1,10 +1,18 @@
 package transfer
 
 import (
+	"errors"
 	"strings"
 	"time"
 
 	"github.com/thalissonfelipe/banking/pkg/domain/entities"
+)
+
+var (
+	errMissingAccDestIDParameter = errors.New("missing account destination id parameter")
+	errMissingAmountParameter    = errors.New("missing amount parameter")
+	errInvalidJSON               = errors.New("invalid json")
+	errAccountOriginDoesNotExist = errors.New("account origin does not exist")
 )
 
 func getTokenFromHeader(authHeader string) string {
@@ -16,6 +24,28 @@ type transferResponse struct {
 	AccountDestinationID string
 	Amount               int
 	CreatedAt            string
+}
+
+type transferRequest struct {
+	AccountDestinationID string
+	Amount               int
+}
+
+type transferCreatedResponse struct {
+	AccountOriginID      string
+	AccountDestinationID string
+	Amount               int
+}
+
+func (t transferRequest) isValid() error {
+	if t.AccountDestinationID == "" {
+		return errMissingAccDestIDParameter
+	}
+	if t.Amount == 0 {
+		return errMissingAmountParameter
+	}
+
+	return nil
 }
 
 func convertTransferToTransferResponse(transfer entities.Transfer) transferResponse {
