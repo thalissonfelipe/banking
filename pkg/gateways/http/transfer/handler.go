2af1d17369d222d0927b8a144cdbeb7c9c76e8a9
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-05-20 19:51:01 -0300

    feat: add jwt middleware on transfer endpoints

--- pkg/gateways/http/transfer/handler.go
+++ pkg/gateways/http/transfer/handler.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/thalissonfelipe/banking/pkg/domain/transfer"
+	"github.com/thalissonfelipe/banking/pkg/gateways/http/middlewares"
 )
 
 type Handler struct {
@@ -14,7 +15,8 @@ type Handler struct {
 func NewHandler(r *mux.Router, usecase transfer.UseCase) *Handler {
 	handler := Handler{usecase: usecase}
 
-	r.HandleFunc("/transfers", handler.ListTransfers).Methods(http.MethodGet)
+	r.Handle("/transfers", middlewares.AuthorizeMiddleware(http.HandlerFunc(handler.ListTransfers))).Methods(http.MethodGet)
+	r.Handle("/transfers", middlewares.AuthorizeMiddleware(http.HandlerFunc(handler.CreateTransfer))).Methods(http.MethodPost)
 
 	return &handler
 }
