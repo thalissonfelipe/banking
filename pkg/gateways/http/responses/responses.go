1e38aece988e1d4fee48fe1bf3b89600a2e86e39
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-05-20 15:24:15 -0300

    fix: content type json was not working

--- pkg/gateways/http/responses/responses.go
+++ pkg/gateways/http/responses/responses.go
@@ -10,13 +10,13 @@ type ErrorResponse struct {
 }
 
 func SendError(w http.ResponseWriter, statusCode int, message string) {
-	w.WriteHeader(statusCode)
 	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(ErrorResponse{Message: message})
 }
 
 func SendJSON(w http.ResponseWriter, statusCode int, payload interface{}) {
-	w.WriteHeader(statusCode)
 	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(payload)
 }
