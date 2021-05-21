2bc6ffb70cad7c707e0de834c155f16c29825d4e
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-05-21 10:19:01 -0300

    feat: add NewRouter function

---
+++ pkg/gateways/http/router.go
@@ -0,0 +1,36 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jackc/pgx/v4"
+
+	acc_usecase "github.com/thalissonfelipe/banking/pkg/domain/account/usecase"
+	tr_usecase "github.com/thalissonfelipe/banking/pkg/domain/transfer/usecase"
+	acc_repo "github.com/thalissonfelipe/banking/pkg/gateways/db/postgres/account"
+	tr_repo "github.com/thalissonfelipe/banking/pkg/gateways/db/postgres/transfer"
+	"github.com/thalissonfelipe/banking/pkg/gateways/hash"
+	acc_handler "github.com/thalissonfelipe/banking/pkg/gateways/http/account"
+	auth_handler "github.com/thalissonfelipe/banking/pkg/gateways/http/auth"
+	tr_handler "github.com/thalissonfelipe/banking/pkg/gateways/http/transfer"
+	"github.com/thalissonfelipe/banking/pkg/services/auth"
+)
+
+func NewRouter(db *pgx.Conn) http.Handler {
+	// Set dependencies
+	hash := hash.Hash{}
+	accountRepo := acc_repo.NewRepository(db)
+	accountUsecase := acc_usecase.NewAccountUseCase(accountRepo, hash)
+	authService := auth.NewAuth(accountUsecase, hash)
+	transferRepo := tr_repo.NewRepository(db)
+	transferUsecase := tr_usecase.NewTransfer(transferRepo, accountUsecase)
+
+	// Register endpoints
+	router := mux.NewRouter()
+	acc_handler.NewHandler(router, accountUsecase)
+	auth_handler.NewHandler(router, authService)
+	tr_handler.NewHandler(router, transferUsecase)
+
+	return router
+}
