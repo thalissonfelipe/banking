d0847e6660c29c2ef1f2ee61a8e7bb889c5bbd1e
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-06-11 00:25:10 -0300

    test: add GetBalanceByID repo tests

---
+++ pkg/gateways/db/mongodb/account/get_balance_by_id_test.go
@@ -0,0 +1,25 @@
+package account
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/thalissonfelipe/banking/pkg/domain/entities"
+	"github.com/thalissonfelipe/banking/pkg/domain/vos"
+)
+
+func TestRepository_GetBalanceByID(t *testing.T) {
+	r := Repository{collection: collection}
+
+	balance, err := r.GetBalanceByID(context.Background(), vos.NewID())
+	assert.Zero(t, balance)
+	assert.ErrorIs(t, err, entities.ErrAccountDoesNotExist)
+
+	acc := createAccount(t, 100)
+
+	balance, err = r.GetBalanceByID(context.Background(), acc.ID)
+	assert.NoError(t, err)
+	assert.Equal(t, acc.Balance, balance)
+}
