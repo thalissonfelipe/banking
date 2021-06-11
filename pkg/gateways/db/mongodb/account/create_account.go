679b84fa090251b0eb62086d52af4a39d6bfad62
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-06-11 14:48:27 -0300

    feat: add CreateAccount repository method

---
+++ pkg/gateways/db/mongodb/account/create_account.go
@@ -0,0 +1,37 @@
+package account
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"github.com/thalissonfelipe/banking/pkg/domain/entities"
+)
+
+func (r Repository) CreateAccount(ctx context.Context, account *entities.Account) error {
+	createdAt := time.Now()
+
+	_, err := r.collection.InsertOne(ctx, bson.D{
+		primitive.E{Key: "id", Value: account.ID},
+		primitive.E{Key: "name", Value: account.Name},
+		primitive.E{Key: "cpf", Value: account.CPF.String()},
+		primitive.E{Key: "secret", Value: account.Secret.String()},
+		primitive.E{Key: "balance", Value: account.Balance},
+		primitive.E{Key: "created_at", Value: createdAt},
+	})
+	if err == nil {
+		account.CreatedAt = createdAt
+
+		return nil
+	}
+
+	if mongo.IsDuplicateKeyError(err) {
+		return entities.ErrAccountAlreadyExists
+	}
+
+	return fmt.Errorf("could not create account: %w", err)
+}
