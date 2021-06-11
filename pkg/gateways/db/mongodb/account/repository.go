b92d5be0d7ca7028e708f6b39ef6071c46bba86c
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-06-11 19:13:32 -0300

    feat: add repo constructor and ensure that repo implements account.Repository

--- pkg/gateways/db/mongodb/account/repository.go
+++ pkg/gateways/db/mongodb/account/repository.go
@@ -1,7 +1,17 @@
 package account
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"github.com/thalissonfelipe/banking/pkg/domain/account"
+)
+
+var _ account.Repository = (*Repository)(nil)
 
 type Repository struct {
 	collection *mongo.Collection
+}
+
+func NewRepository(collection *mongo.Collection) Repository {
+	return Repository{collection: collection}
 }
