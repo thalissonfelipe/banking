42952052e5ec5f20c574712382a9965ecb9adfae
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-06-11 00:24:31 -0300

    feat: add GetBalanceByID repo method

---
+++ pkg/gateways/db/mongodb/account/get_balance_by_id.go
@@ -0,0 +1,31 @@
+package account
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/thalissonfelipe/banking/pkg/domain/entities"
+	"github.com/thalissonfelipe/banking/pkg/domain/vos"
+)
+
+func (r Repository) GetBalanceByID(ctx context.Context, accountID vos.ID) (int, error) {
+	opts := options.FindOne().SetProjection(bson.M{"balance": 1})
+
+	var account entities.Account
+
+	err := r.collection.FindOne(ctx, bson.M{"id": accountID}, opts).Decode(&account)
+	if err == nil {
+		return account.Balance, nil
+	}
+
+	if errors.Is(err, mongo.ErrNoDocuments) {
+		return 0, entities.ErrAccountDoesNotExist
+	}
+
+	return 0, fmt.Errorf("could not get account balance: %w", err)
+}
