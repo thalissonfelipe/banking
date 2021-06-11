ee042b5161a4d87381c6991ed9fad4abeec623e1
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-06-11 19:05:19 -0300

    test: add GetAccountByCPF repo tests

---
+++ pkg/gateways/db/mongodb/account/get_account_by_cpf_test.go
@@ -0,0 +1,32 @@
+package account
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/thalissonfelipe/banking/pkg/domain/entities"
+	"github.com/thalissonfelipe/banking/pkg/tests/testdata"
+)
+
+func TestRepostory_GetAccountByCPF(t *testing.T) {
+	r := Repository{collection: collection}
+
+	defer dropCollection(t, collection)
+
+	account, err := r.GetAccountByCPF(context.Background(), testdata.GetValidCPF())
+	assert.Nil(t, account)
+	assert.ErrorIs(t, err, entities.ErrAccountDoesNotExist)
+
+	acc := createAccount(t, 100)
+
+	account, err = r.GetAccountByCPF(context.Background(), acc.CPF)
+	assert.NoError(t, err)
+
+	assert.Equal(t, acc.ID, account.ID)
+	assert.Equal(t, acc.Name, account.Name)
+	assert.Equal(t, acc.CPF, account.CPF)
+	assert.Equal(t, acc.Balance, account.Balance)
+	assert.NotEmpty(t, account.Secret)
+}
