387c9debb2c8dd61b2d6cb1c9cc3250e4ebc54a0
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-06-13 11:08:25 -0300

    feat: add CreateTransfer repo method (mongo)

---
+++ pkg/gateways/db/mongodb/transfer/create_transfer.go
@@ -0,0 +1,80 @@
+package transfer
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"github.com/thalissonfelipe/banking/pkg/domain/entities"
+	"github.com/thalissonfelipe/banking/pkg/domain/vos"
+)
+
+func (r Repository) CreateTransfer(ctx context.Context, transfer *entities.Transfer) error {
+	transfersCollection := r.db.Collection("transfers")
+	accountCollection := r.db.Collection("accounts")
+
+	callback := func(sc mongo.SessionContext) (interface{}, error) {
+		err := r.updateBalance(ctx, accountCollection, transfer.AccountOriginID, -transfer.Amount)
+		if err != nil {
+			return nil, err
+		}
+
+		err = r.updateBalance(ctx, accountCollection, transfer.AccountDestinationID, transfer.Amount)
+		if err != nil {
+			return nil, err
+		}
+
+		err = r.createTransfer(ctx, transfersCollection, transfer)
+		if err != nil {
+			return nil, err
+		}
+
+		return nil, nil
+	}
+
+	session, err := r.db.Client().StartSession()
+	if err != nil {
+		return fmt.Errorf("could not start a new session: %w", err)
+	}
+	defer session.EndSession(ctx)
+
+	_, err = session.WithTransaction(ctx, callback)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r Repository) updateBalance(ctx context.Context, collection *mongo.Collection, id vos.ID, amount int) error {
+	filter := bson.M{"id": id}
+	update := bson.D{primitive.E{Key: "$inc", Value: bson.D{primitive.E{Key: "balance", Value: amount}}}}
+
+	_, err := collection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return fmt.Errorf("could not update balance: %w", err)
+	}
+
+	return nil
+}
+
+func (r Repository) createTransfer(ctx context.Context, collection *mongo.Collection, transfer *entities.Transfer) error {
+	transfer.CreatedAt = time.Now()
+
+	_, err := collection.InsertOne(ctx, bson.D{
+		primitive.E{Key: "id", Value: transfer.ID},
+		primitive.E{Key: "account_origin_id", Value: transfer.AccountOriginID},
+		primitive.E{Key: "account_destination_id", Value: transfer.AccountDestinationID},
+		primitive.E{Key: "amount", Value: transfer.Amount},
+		primitive.E{Key: "created_at", Value: transfer.CreatedAt},
+	})
+	if err != nil {
+		return fmt.Errorf("could not create transfer: %w", err)
+	}
+
+	return nil
+}
