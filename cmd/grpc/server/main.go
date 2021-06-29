f5332b62dbffcb66a7d921c23925569a7595e3c9
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-06-29 18:16:40 -0300

    feat: add grpc server with repository in memory

---
+++ cmd/grpc/server/main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	accUsecase "github.com/thalissonfelipe/banking/pkg/domain/account/usecase"
+	"github.com/thalissonfelipe/banking/pkg/domain/entities"
+	trUsecase "github.com/thalissonfelipe/banking/pkg/domain/transfer/usecase"
+	"github.com/thalissonfelipe/banking/pkg/domain/vos"
+	grpcServer "github.com/thalissonfelipe/banking/pkg/gateways/grpc"
+	"github.com/thalissonfelipe/banking/pkg/services/auth"
+	"github.com/thalissonfelipe/banking/pkg/tests/testdata"
+	proto "github.com/thalissonfelipe/banking/proto/banking"
+)
+
+type InMemoryAccountDatabase struct {
+	Accounts []entities.Account
+}
+
+func (i InMemoryAccountDatabase) GetAccounts(ctx context.Context) ([]entities.Account, error) {
+	return i.Accounts, nil
+}
+
+func (i InMemoryAccountDatabase) GetBalanceByID(ctx context.Context, id vos.AccountID) (int, error) {
+	for _, acc := range i.Accounts {
+		if acc.ID == id {
+			return acc.Balance, nil
+		}
+	}
+
+	return 0, entities.ErrAccountDoesNotExist
+}
+
+func (i *InMemoryAccountDatabase) CreateAccount(ctx context.Context, account *entities.Account) error {
+	for _, acc := range i.Accounts {
+		if acc.CPF == account.CPF {
+			return entities.ErrAccountAlreadyExists
+		}
+	}
+
+	i.Accounts = append(i.Accounts, *account)
+
+	return nil
+}
+
+func (i InMemoryAccountDatabase) GetAccountByCPF(ctx context.Context, cpf vos.CPF) (entities.Account, error) {
+	for _, acc := range i.Accounts {
+		if acc.CPF == cpf {
+			return acc, nil
+		}
+	}
+
+	return entities.Account{}, entities.ErrAccountDoesNotExist
+}
+
+func (i InMemoryAccountDatabase) GetAccountByID(ctx context.Context, id vos.AccountID) (entities.Account, error) {
+	for _, acc := range i.Accounts {
+		if acc.ID == id {
+			return acc, nil
+		}
+	}
+
+	return entities.Account{}, entities.ErrAccountDoesNotExist
+}
+
+type InMemoryEncrypter struct{}
+
+func (i InMemoryEncrypter) Hash(secret string) ([]byte, error) {
+	return []byte(secret), nil
+}
+
+func (i InMemoryEncrypter) CompareHashAndSecret(hashedSecret, secret []byte) error {
+	return nil
+}
+
+type InMemoryTransferDatabase struct {
+	Transfers []entities.Transfer
+}
+
+func (i InMemoryTransferDatabase) GetTransfers(ctx context.Context, id vos.AccountID) ([]entities.Transfer, error) {
+	return i.Transfers, nil
+}
+
+func (i *InMemoryTransferDatabase) CreateTransfer(ctx context.Context, transfer *entities.Transfer) error {
+	tr := entities.NewTransfer(transfer.AccountOriginID, transfer.AccountDestinationID, transfer.Amount)
+
+	i.Transfers = append(i.Transfers, tr)
+
+	return nil
+}
+
+func main() {
+	creds, err := credentials.NewServerTLSFromFile("../cert/server.crt", "../cert/server.key")
+	if err != nil {
+		log.Fatalf("Failed to setup tls: %v", err)
+	}
+
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	acc := entities.NewAccount("Felipe", testdata.GetValidCPF(), testdata.GetValidSecret())
+	acc.Balance = 1000
+	accRepo := &InMemoryAccountDatabase{Accounts: []entities.Account{acc}}
+	trRepo := &InMemoryTransferDatabase{}
+	enc := &InMemoryEncrypter{}
+	accountUsecase := accUsecase.NewAccountUsecase(accRepo, enc)
+	transferUsecase := trUsecase.NewTransferUsecase(trRepo, accountUsecase)
+	auth := auth.NewAuth(accountUsecase, enc)
+
+	srv := grpcServer.NewServer(accountUsecase, transferUsecase, auth)
+
+	s := grpc.NewServer(
+		grpc.Creds(creds),
+		grpc.UnaryInterceptor(grpcServer.AuthInterceptor),
+	)
+
+	proto.RegisterBankingServiceServer(s, srv)
+
+	log.Println("Server listening on localhost:9000!")
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
