713b815b8ac0325a4ca769545c406dbf68886ef1
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-06-29 15:54:34 -0300

    feat: add jwt middleware

---
+++ pkg/gateways/grpc/auth_interceptor.go
@@ -0,0 +1,53 @@
+package grpc
+
+import (
+	"context"
+
+	"github.com/thalissonfelipe/banking/pkg/services/auth"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+// jwtMethods is an array of methods that need authentication validation.
+var jwtMethods = []string{
+	"/banking.BankingService/GetTransfers",
+	"/banking.BankingService/CreateTransfer",
+}
+
+func AuthInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	ok := needAuthentication(info.FullMethod)
+	if !ok {
+		return handler(ctx, req)
+	}
+
+	meta, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, status.Error(codes.Unauthenticated, "missing context metadata")
+	}
+
+	if len(meta["authorization"]) != 1 {
+		return nil, status.Error(codes.Unauthenticated, "invalid token")
+	}
+
+	token := meta["authorization"][0]
+	err := auth.IsValidToken(token)
+	if err != nil {
+		return nil, status.Error(codes.Unauthenticated, "incorrect access token")
+	}
+
+	return handler(ctx, req)
+}
+
+// needAuthentication will check if the provided method need authentication validation. Only transfers
+// methods will need jwt authentication.
+func needAuthentication(method string) bool {
+	for _, m := range jwtMethods {
+		if m == method {
+			return true
+		}
+	}
+
+	return false
+}
